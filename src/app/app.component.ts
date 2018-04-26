3564d79cd835cd95a1ae37f01b6070e987b11627
Mauricio Grejanin <mgrejanin@eurekaria.com>
2018-04-26 01:00:39 -0300

    Revert "Revert "Login ok""
    This reverts commit fb66ab9f2def9a143ea8620137a5805ef45d4e38.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-
 import { Platform } from 'ionic-angular';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ParticipanteService } from '../shared/participante/participante.service';
 import { Home } from '../pages/home/home';
-<<<<<<< HEAD
 import { LoginPage } from '../pages/login/login';
-=======
-import { FacebookLoginPage } from '../pages/facebook-login/facebook-login';
->>>>>>> parent of 1f7cc39... Login ok
 
 
 @Component({
@@ -20,11 +14,7 @@ import { FacebookLoginPage } from '../pages/facebook-login/facebook-login';
 export class MyApp {
 
   // make Home the root (or first) page
-<<<<<<< HEAD
   rootPage = LoginPage;
-=======
-  rootPage = FacebookLoginPage;
->>>>>>> parent of 1f7cc39... Login ok
 
   constructor(
     public platform: Platform,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,33 +8,31 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { ParticipanteService } from '../shared/participante/participante.service';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FacebookLoginPage } from '../pages/facebook-login/facebook-login';
+import { LoginPage } from '../pages/login/login';
 import { Facebook } from '@ionic-native/facebook';
-import { HTTP } from '@ionic-native/http';
 import { IonicStorageModule } from '@ionic/storage';
-import { FacebookLoginPageModule } from '../pages/facebook-login/facebook-login.module';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 
 @NgModule({
   declarations: [
     MyApp,
     Home,
-    // FacebookLoginPage
-    
+    LoginPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     HttpModule,
     BrowserAnimationsModule,
-    IonicStorageModule.forRoot(),
-    FacebookLoginPageModule
+    IonicStorageModule.forRoot({
+      name: 'cartolando'
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     Home,
-    FacebookLoginPage
+    LoginPage
   ],
   providers: [
     StatusBar,
